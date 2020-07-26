c41201cd0aa09d5c94fe6e658f99e041d689f9b9
YamanQD <yamanqassas99@gmail.com>
2020-07-26 22:46:52 +0300

    Added mail deliverability check.

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
+from validate_email import validate_email as validate_mail
 
 
 class RegistrationForm(FlaskForm):
@@ -18,8 +19,15 @@ class RegistrationForm(FlaskForm):
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
+        is_valid = validate_mail(email_address=email.data, \
+        check_regex=True, check_mx=True, \
+        from_address='my@from.addr.ess', helo_host='my.host.name', \
+        smtp_timeout=10, dns_timeout=10, use_blacklist=True)
+
         if user:
             raise ValidationError('That email is taken, Please choose a different one.')
+        if not is_valid:
+            raise ValidationError('That email is not valid. Please choose a valid email.')
 
 
 
